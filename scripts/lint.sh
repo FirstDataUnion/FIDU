6a348db40ac8e325d24e5738c05cf2da282fdc8f
Oli <evenoli@hotmail.co.uk>
2025-05-27 15:32:27 -0400

    example justification of some lint skipping herasy

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -6,6 +6,28 @@
 # run with './scripts/lint.sh'
 # After running the setup_dev.sh script, this script will be ran automatically as a pre-commit hook
 
+# Currently this script will exit on any error, which will cancel the pre-commit hook. 
+# Could be a good space to work on, to allow the pre-commit hook to continue even if there are certain errors
+# or warnings present, so the linter is less of a pain. 
+
+# Function to handle script exit
+handle_exit() {
+    if [ $? -ne 0 ]; then
+        echo """
+❌ Code quality checks failed!
+
+YOUR COMMIT HAS BEEN REJECTED BY THE COUNCIL OF LINTERS.
+Please fix the issues above before committing.
+If you need to bypass these checks temporarily, use:
+    git commit --no-verify -m "your message"
+"""
+        exit 1
+    fi
+}
+
+# Set up trap to catch script exit
+trap handle_exit EXIT
+
 # Exit on any error
 set -e
 
